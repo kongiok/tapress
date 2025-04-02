0686c5ed5171efce421d2ca9332f86c94ad8e0da
kongiok <me@ong.kongiok.tw>
2025-04-02 15:06:27 +0800

    fix: legacy codes not suite for current framework and add a test suite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    coverage: {
+      reporter: ["text", "html"],
+    },
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
+  },
 });
