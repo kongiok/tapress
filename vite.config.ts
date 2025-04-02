aff5e47833a9c1cbf671ee3f2980aeca3ad60413
kongiok <me@ong.kongiok.tw>
2025-04-02 15:13:44 +0800

    fix: config function import error

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
@@ -8,9 +8,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
-    coverage: {
-      reporter: ["text", "html"],
-    },
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
   },
 });
